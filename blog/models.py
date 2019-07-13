80e4d696420d8f8c20bdea4e3328b7a9b7cfbc91
evyhaan <evy.haan@gmail.com>
2019-07-12 21:33:22 -0700

    all hooked up

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.CharField(max_length=2000)
     image = models.ImageField(upload_to='images/blog')
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_short(self):
+        return self.pub_date.strftime('%B %e, %Y')
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects
+    context = {'blogs': blogs}
+    return render(request, 'blog/all_blogs.html', context)
+
+
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    context = {'blog':blog}
+    return render(request, 'blog/detail.html', context)
