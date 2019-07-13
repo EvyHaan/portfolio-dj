f425e1d0623ffb8281fa7c8f258b00bbc6a9a4f1
evyhaan <evy.haan@gmail.com>
2019-07-12 21:50:18 -0700

    final fiddles before messing with the admin page

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,6 @@ class Job(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/projects')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
